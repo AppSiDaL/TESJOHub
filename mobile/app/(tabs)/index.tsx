9c4e86dd053af34a9559a6a4e4fbab9419c4b866
AppSiDaL <gilielcrack343@gmail.com>
2024-06-01 07:16:41 -0600

    chore: Add profile tab to TabLayout component

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,70 +1,178 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
+  const Posts = [
+    {
+      id: "1",
+      userName: "Jenny Doe",
+      userImg: "https://avatars.githubusercontent.com/u/6820?s=400&u=3b7f6b2f",
+      postTime: "4 mins ago",
+      post: "Hey there, this is my test for a post of my social app in React Native.",
+      postImg:
+        "https://media.publit.io/file/ZKyHDhnApWjkCODtX74IqkhrL52oOdJrMypbBaLZin09f42tuaA/Partidos-politicos-Mexico.jpg",
+      liked: true,
+      likes: "14",
+      comments: [
+        {
+          id: "1",
+          userName: "John Doe",
+          commentText: "This is hilarious!",
+        },
+        {
+          id: "2",
+          userName: "Ken William",
+          commentText: "I know right?!",
+        },
+        {
+          id: "3",
+          userName: "Selina Paul",
+          commentText: "Wow!",
+        },
+      ],
+    },
+    {
+      id: "2",
+      userName: "John Doe",
+      userImg: "https://avatars.githubusercontent.com/u/6820?s=400&u=3b7f6b2f",
+      postTime: "2 hours ago",
+      post: "Hey there, this is my test for a post of my social app in React Native.",
+      postImg: "none",
+      liked: false,
+      likes: "8",
+      comments: [],
+    },
+    {
+      id: "3",
+      userName: "Ken William",
+      userImg: "https://avatars.githubusercontent.com/u/6820?s=400&u=3b7f6b2f",
+      postTime: "1 hours ago",
+      post: "Hey there, this is my test for a post of my social app in React Native.",
+      postImg:
+        "https://media.publit.io/file/ZKyHDhnApWjkCODtX74IqkhrL52oOdJrMypbBaLZin09f42tuaA/FVT7HYTJF2SVT0C.webp",
+      liked: true,
+      likes: "1",
+      comments: [],
+    },
+    {
+      id: "4",
+      userName: "Selina Paul",
+      userImg: "https://avatars.githubusercontent.com/u/6820?s=400&u=3b7f6b2f",
+      postTime: "1 day ago",
+      post: "Hey there, this is my test for a post of my social app in React Native.",
+      postImg:
+        "https://media.publit.io/file/ZKyHDhnApWjkCODtX74IqkhrL52oOdJrMypbBaLZin09f42tuaA/unnamed.png",
+      liked: true,
+      likes: "22",
+      comments: [
+        {
+          id: "1",
+          userName: "Ken William",
+          commentText: "This is hilarious!",
+        },
+        {
+          id: "2",
+          userName: "Jenny Doe",
+          commentText: "I know right?!",
+        },
+      ],
+    },
+  ];
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+    >
+      <ThemedView>
+        {Posts.map((item, index) => (
+          <ThemedView key={index} style={styles.postContainer}>
+            <ThemedView style={styles.postHeader}>
+              <ThemedView style={styles.userInfo}>
+                <Image
+                  style={styles.userImg}
+                  source={{ uri: item.userImg }}
+                />
+                <ThemedView>
+                  <ThemedText style={styles.userName}>{item.userName}</ThemedText>
+                  <ThemedText style={styles.postTime}>{item.postTime}</ThemedText>
+                </ThemedView>
+              </ThemedView>
+              <ThemedText>...</ThemedText>
+            </ThemedView>
+            <ThemedText style={styles.post}>{item.post}</ThemedText>
+            {item.postImg !== "none" ? (
+              <Image
+                source={{ uri: item.postImg }}
+                style={styles.postImg}
+                resizeMode="cover"
+              />
+            ) : (
+              <ThemedView />
+            )}
+            <ThemedView style={styles.postFooter}>
+              <ThemedView style={styles.postFooter}>
+                <ThemedText style={styles.postLikes}>{item.likes} Likes</ThemedText>
+              </ThemedView>
+              <ThemedView style={styles.postFooter}>
+                <ThemedText style={styles.postLikes}>{item.comments.length} comments</ThemedText>
+              </ThemedView>
+            </ThemedView>
+          </ThemedView>
+        ))}
       </ThemedView>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  postContainer: {
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+  postHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  userInfo: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  userImg: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  userName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  postTime: {
+    fontSize: 12,
+  },
+  post: {
+    marginTop: 10,
+    fontSize: 14,
+  },
+  postImg: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  postFooter: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  postLikes: {
+    fontSize: 14,
   },
 });
